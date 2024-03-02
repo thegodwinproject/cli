62f8346178356aa2592e1f3d0c54c2e4cebc3d8c
Rod Watkins <rodwatkins@outlook.com>
2024-03-02 08:49:18 -0800

    removed test of installing old versions

--- internal/testhelpers/testhelpers_test.go
+++ internal/testhelpers/testhelpers_test.go
@@ -1,9 +1,7 @@
 package testhelpers_test
 
 import (
-	"errors"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"runtime"
 	"testing"
@@ -27,50 +25,51 @@ func Test_EnsureBuffaloCMD(t *testing.T) {
 	r.FileExists(path)
 }
 
-func Test_InstallOldBuffaloCMD(t *testing.T) {
-	if runtime.GOOS != "linux" {
-		// this test calls api.github.com too much.
-		// no reason for linux, but just reduce the calls as 1/3
-		t.Skip("skipping Test_InstallOldBuffaloCMD (non-linux env)")
-	}
-
-	tt := []struct {
-		name    string
-		version string
-		err     error
-	}{
-		{
-			name:    "non-existing version",
-			version: "v0.16.40",
-			err:     errors.New("unknown gobuffalo cli version v0.16.40"),
-		},
-		{
-			name:    "existing version",
-			version: "v0.18.1",
-			err:     nil,
-		},
-		{
-			name:    "existing old version",
-			version: "v0.16.27",
-			err:     nil,
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			r := require.New(t)
-
-			err := testhelpers.InstallOldBuffaloCMD(t, tc.version)
-			if tc.err != nil {
-				r.EqualError(err, tc.err.Error())
-				return
-			}
-
-			r.NoError(err)
-			cmd := exec.Command("buffalo", "version")
-			out, err := cmd.CombinedOutput()
-			r.NoError(err)
-			r.Contains(string(out), tc.version)
-		})
-	}
-}
+//
+//func Test_InstallOldBuffaloCMD(t *testing.T) {
+//	if runtime.GOOS != "linux" {
+//		// this test calls api.github.com too much.
+//		// no reason for linux, but just reduce the calls as 1/3
+//		t.Skip("skipping Test_InstallOldBuffaloCMD (non-linux env)")
+//	}
+//
+//	tt := []struct {
+//		name    string
+//		version string
+//		err     error
+//	}{
+//		{
+//			name:    "non-existing version",
+//			version: "v0.16.40",
+//			err:     errors.New("unknown gobuffalo cli version v0.16.40"),
+//		},
+//		{
+//			name:    "existing version",
+//			version: "v0.18.1",
+//			err:     nil,
+//		},
+//		{
+//			name:    "existing old version",
+//			version: "v0.16.27",
+//			err:     nil,
+//		},
+//	}
+//
+//	for _, tc := range tt {
+//		t.Run(tc.name, func(t *testing.T) {
+//			r := require.New(t)
+//
+//			err := testhelpers.InstallOldBuffaloCMD(t, tc.version)
+//			if tc.err != nil {
+//				r.EqualError(err, tc.err.Error())
+//				return
+//			}
+//
+//			r.NoError(err)
+//			cmd := exec.Command("buffalo", "version")
+//			out, err := cmd.CombinedOutput()
+//			r.NoError(err)
+//			r.Contains(string(out), tc.version)
+//		})
+//	}
+//}
